1bf4d33afc8e8993da28319e9f94dfe15c152d24
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-08-21 22:06:11 +0200

    Add logging to Initializer

--- web/src/main/java/com/chriswk/collection/config/CollectionInitializer.java
+++ web/src/main/java/com/chriswk/collection/config/CollectionInitializer.java
@@ -1,5 +1,7 @@
 package com.chriswk.collection.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -8,12 +10,15 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
 public class CollectionInitializer implements WebApplicationInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CollectionInitializer.class);
 	public void onStartup(ServletContext servletContext) {
-		AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
+		LOGGER.info("Starting up");
+        AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
 		mvcContext.register(CollectionMvcConfig.class);
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(mvcContext));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
+        LOGGER.info("Done loading");
 	}
 	
 }

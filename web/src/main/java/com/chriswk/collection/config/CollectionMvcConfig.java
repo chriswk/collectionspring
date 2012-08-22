a97b5dc867bb0ab2693768109dd8c847661bdbb9
Christopher Kolstad <Christopher.Kolstad@iteraconsulting.no>
2012-08-22 08:27:52 +0200

    Set up scalate view resolver

--- web/src/main/java/com/chriswk/collection/config/CollectionMvcConfig.java
+++ web/src/main/java/com/chriswk/collection/config/CollectionMvcConfig.java
@@ -1,7 +1,10 @@
 package com.chriswk.collection.config;
 
+import org.fusesource.scalate.spring.view.ScalateViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -10,11 +13,23 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages = "com.chriswk.collection")
 public class CollectionMvcConfig {
 
-	public InternalResourceViewResolver configureInteralResourceViewResolver() {
+    @Bean
+	public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/jsp");
         resolver.setSuffix(".jsp");
+        resolver.setOrder(2);
         return resolver;
     }
+
+    @Bean
+    public ViewResolver scalateViewResolver() {
+        ScalateViewResolver resolver = new ScalateViewResolver();
+        resolver.setPrefix("/WEB-INF/jade");
+        resolver.setSuffix(".jade");
+        resolver.setOrder(1);
+        return resolver;
+    }
+
 
 }
